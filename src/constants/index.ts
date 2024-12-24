44ea93512a9afd3d06c5770768c38d48defbf11b
moinuddinrao <moinrajput594@gmail.com>
2024-12-24 15:33:48 +0500

    feat: add responsive navbar component with navigation links

---
+++ src/constants/index.ts
@@ -0,0 +1,24 @@
+// Desc: Constants for the project
+
+export const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    href: "#home",
+  },
+  {
+    id: 2,
+    name: "About",
+    href: "#about",
+  },
+  {
+    id: 3,
+    name: "Work",
+    href: "#work",
+  },
+  {
+    id: 4,
+    name: "Contact",
+    href: "#contact",
+  },
+];
